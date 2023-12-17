989029446e6407cb6fb39e3c5cb25063a4f6bf25
Kishobana <154045358+Kishobana@users.noreply.github.com>
2023-12-17 14:03:59 +0530

    Update Regist.php

--- Regist.php
+++ Regist.php
@@ -26,8 +26,8 @@
         </select><br><br>
         Gender:
         <input type="radio" name="gender" value="Male" required> Male
-        <input type="radio" name="gender" value="Female" required> Female<br><br>
-        Password: <input type="password" name="password" required><br><br>
+        <input type="radio" name="gender" value="Female" > Female<br><br>
+        Password: <input type="password" name="password" ><br><br>
         <input type="submit" value="Register">
     </form>
 </body>
